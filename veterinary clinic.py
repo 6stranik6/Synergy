5efd7ae2bd7de981f59eab1f469436a97808c0dd
6stranik6 <6stranik6@mail.ru>
2023-12-15 20:48:58 +0300

    Made

---
+++ veterinary clinic.py
@@ -0,0 +1,4 @@
+type_of_pet = input('Видите вид питомца ')
+nickname = input('Ввидите кличку питомца ')
+age = input('Ввидите возраст питомца ')
+print ('Это' , type_of_pet , 'по кличке', nickname, '.', 'Возраст:', age)
